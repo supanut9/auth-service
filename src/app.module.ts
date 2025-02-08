cc7362150c0f7a32e39bf05f38b7ef1a75a9b922
supanut <supanut.tanso@gmail.com>
2025-02-08 10:54:08 +0700

    feat(database): connect mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './configs/database.config';
+import { DataAccessLayerModule } from './modules/data-access-layer/data-access-layer.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    }),
+    DataAccessLayerModule,
+  ],
   controllers: [],
   providers: [],
 })
